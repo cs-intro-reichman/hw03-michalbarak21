1f2c63c935773a38026238716bbd1d5084b553ff
Michal Barak <michal.barak01@post.runi.ac.il>
2024-01-08 18:14:13 +0200

    submitted code for Q2

--- LowerCase.java
+++ LowerCase.java
@@ -11,7 +11,16 @@ public class LowerCase {
     * Non-letter characters are left as is.
     */
     public static String lowerCase(String s) {
-        // Replace the following statement with your code
-        return null;
+        String answer = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                answer = answer + ' '; 
+            } else if (s.charAt(i) >= 65 && s.charAt(i) <= 90) {
+                answer = answer + (char) (s.charAt(i) + 32) ; 
+            } else {
+                answer = answer + s.charAt(i);
+            }
+        }
+        return answer;
     }
 }
